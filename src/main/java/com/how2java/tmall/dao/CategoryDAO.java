5985a036db0f52e8763c7f8d5241bc2c8234f28f
shuiyuyue <shuiyuyue@outlook.com>
2019-06-10 22:16:02 +0800

    啦啦啦
    Signed-off-by: shuiyuyue <shuiyuyue@outlook.com>

--- src/main/java/com/how2java/tmall/dao/CategoryDAO.java
+++ src/main/java/com/how2java/tmall/dao/CategoryDAO.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 //CategoryDAO 类集成了 JpaRepository，就提供了CRUD和分页 的各种常见功能。 这就是采用 JPA 方便的地方~
 public interface CategoryDAO extends JpaRepository<Category,Integer> {
+
 }
